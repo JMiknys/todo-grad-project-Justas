2604a1e98502184e8fdb01b5d19e12e2c12d751a
Justas Miknys <jmiknys@scottlogic.co.uk>
2016-08-11 11:23:13 +0100

    Deleted some unused code

--- server/server.js
+++ server/server.js
@@ -61,11 +61,6 @@ module.exports = function(port, middleware, callback) {
     });
 
     function getTodo(id) {
-        /*
-        return todos.filter(function(todo) {
-            return todo.id === id;
-        })[0];
-        */
         return _.find(todos, function(todo) {
             return todo.id === id;
         });
